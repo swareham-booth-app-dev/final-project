77cfd5f97db278a69949de8fb2c40828f1180dc6
srwareham <srwareham@hotmail.com>
2021-05-22 20:19:48 +0000

    Require sign in first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action(:load_current_user)
   
   # Uncomment this if you want to force users to sign in before any other actions
-  # before_action(:force_user_sign_in)
+  before_action(:force_user_sign_in)
   
   def load_current_user
     the_id = session[:user_id]
